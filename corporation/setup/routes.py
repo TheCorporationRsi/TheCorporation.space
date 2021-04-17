961fe5c7b69a7ba4bf9d6d93975a569a7ea3836a
gata1526 <gata23laroche@gmail.com>
2021-04-17 10:11:19 -0400

    ok

--- corporation/setup/routes.py
+++ corporation/setup/routes.py
@@ -8,7 +8,6 @@ import requests
 import json
 import asyncio
 
-from flask import Blueprint
 import discord
 from discord.ext import ipc
 
@@ -23,19 +22,20 @@ setup = Blueprint('setup', __name__)
 server_id = 831248117571649566
 
 
-#================================================= Role =========================================================
+# ================================================= Role =========================================================
 @setup.route("/discord/role_setup", defaults={"department": 0, "division": 0}, methods=['GET', 'POST'])
 @setup.route("/discord/role_setup/<int:department>", defaults={"division": 0}, methods=['GET', 'POST'])
 @setup.route("/discord/role_setup/<int:department>/<int:division>", methods=['GET', 'POST'])
 @login_required
 def role_setup(department, division):
-    
+
     discord_role = asyncio.run(discord_bot.request("get_roles"))
     for role in discord_role:
         print(role)
 
+
     if division > 0:
-        if not current_user.is_manager(division = division):
+        if not current_user.is_manager(division=division):
             return redirect(url_for('main.home'))
     else:
         if not current_user.is_manager(department = department, division = division):
@@ -53,7 +53,7 @@ def role_setup(department, division):
         db.session.add(role)
         db.session.commit()
         flash('Role has been created!', 'success')
-        return redirect(url_for('setup.role_setup'))
+        return redirect(url_for('managers.role_manager'))
     
     if department == 0 and division == 0:
         roles = Role.query.order_by(Role.title).all()
@@ -66,4 +66,4 @@ def role_setup(department, division):
     divisions = Division.query.order_by(Division.title).all()
     departments = Department.query.order_by(Department.title).all()
     
-    return render_template("setup/role_setup.html", title = "Role setup", roles = roles, Role = Role,  form=form, divisions = divisions, departments = departments, currentdiv = division, currentdep = department)
+    return render_template("managers/role_manager.html", title = "Role manager", roles = roles, Role = Role,  form=form, divisions = divisions, departments = departments, currentdiv = division, currentdep = department)
