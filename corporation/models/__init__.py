d467b43f327b478c4dbceb314b5eb6fda8c33bc5
cyber-dreamer <gata23laroche@gmail.com>
2021-07-18 15:33:20 +0000

    save

---
+++ corporation/models/__init__.py
@@ -0,0 +1,3 @@
+
+
+from corporation.models.main_models import User, Rolevsuser, Role, Division, Department, Webpage_template, Post, Message, Tribute, Influence_rank, Influence, Influence_transaction, Logs
