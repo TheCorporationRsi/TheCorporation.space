207052252f061144e25649118ee66eb47d71e37b
Cyber-Dreamer <gata23laroche@gmail.com>
2024-12-13 22:21:47 -0500

    save

--- flutter_dashboard/lib/widgets/dashboard_pages/home/structure_widget.dart
+++ flutter_dashboard/lib/widgets/dashboard_pages/home/structure_widget.dart
@@ -1,15 +1,14 @@
+import 'package:corp_api/corp_api.dart';
 import 'package:flutter_dashboard/util/responsive.dart';
 import 'package:flutter_dashboard/widgets/dashboard_pages/Influence_system/components/influence_details_card.dart';
-import 'package:flutter_dashboard/widgets/dashboard_pages/components/bar_graph_widget.dart';
 import 'package:flutter_dashboard/widgets/dashboard_pages/components/line_chart_card.dart';
-import 'package:flutter_dashboard/widgets/header/profile_widget.dart';
-import 'package:flutter_dashboard/widgets/dashboard_pages/components/custom_card_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:built_collection/built_collection.dart';
 import 'package:flutter_dashboard/const/constant.dart';
-import 'package:video_player/video_player.dart';
-import 'package:flutter_dashboard/util/video_player_widget.dart';
+import 'package:flutter_dashboard/main.dart';
+
+import 'package:flutter_dashboard/util/css_color.dart';
+import 'package:flutter_dashboard/util/icon_helper.dart';
 
 class StructureWidget extends StatefulWidget {
   const StructureWidget({super.key});
@@ -19,132 +18,343 @@ class StructureWidget extends StatefulWidget {
 }
 
 class _StructureWidgetState extends State<StructureWidget> {
+  BuiltList<GetDepartments200ResponseInner> departments =
+      BuiltList<GetDepartments200ResponseInner>();
+  Map<int, bool> _dropdownOpen = {};
+  Map<int, GlobalKey<FormState>> _formKeys = {};
 
-  final List<Map<String, dynamic>> departments = [
-    {
-      'name': 'Human Resources',
-      'divisions': ['Recruitment', 'Employee Relations', 'Training & Development']
-    },
-    {
-      'name': 'Finance',
-      'divisions': ['Accounting', 'Budgeting', 'Payroll']
-    },
-    {
-      'name': 'Marketing',
-      'divisions': ['Advertising', 'Market Research', 'Public Relations']
-    },
-    {
-      'name': 'IT',
-      'divisions': ['Infrastructure', 'Software Development', 'Support']
-    },
-    {
-      'name': 'Operations',
-      'divisions': ['Logistics', 'Quality Control', 'Production']
-    },
-    {
-      'name': 'Operations',
-      'divisions': ['Logistics', 'Quality Control', 'Production']
-    },
-    {
-      'name': 'Operations',
-      'divisions': ['Logistics', 'Quality Control', 'Production']
-    },
-    {
-      'name': 'Operations',
-      'divisions': ['Logistics', 'Quality Control', 'Production']
-    },
-    {
-      'name': 'Operations',
-      'divisions': ['Logistics', 'Quality Control', 'Production']
-    },
-  ];
+  bool _isLoading = true;
+  final corpStructureClient = corpApi.getStructureApi();
+
+  Future<void> _initialize() async {
+    try {
+      final response = await corpStructureClient.getDepartments();
+      if (response.data != null) {
+        departments = response.data ?? departments;
+      }
+    } catch (error) {
+      print(error);
+    }
+    setState(() {
+      _isLoading = false;
+    });
+  }
 
   @override
   void initState() {
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
+    _initialize();
+    for (int i = 0; i < departments.length; i++) {
+      _formKeys[i] = GlobalKey<FormState>();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return Column(
-                children: [
-                  const SizedBox(height: 10),
-                  Text('The Corporation Structure',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.orbitron(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      )),
-                  const SizedBox(height: 20),
-                  GridView(
-                    shrinkWrap: true,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: constraints.maxWidth > 1000 ? 3 : 2,
-                        crossAxisSpacing: 20,
-                        mainAxisSpacing: 20,
-                        mainAxisExtent: 500),
-                    children: [
-                      for (var department in departments)
-                        CustomCard(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SvgPicture.asset(
-                                'assets/logo/department.svg',
-                                colorFilter: ColorFilter.mode(
-                                  Colors.grey.withOpacity(1.0), // Change this to your desired color and opacity
-                                  BlendMode.srcIn,
-                                ),
-                                width: 200,
-                                height: 200,
-                              ),
-                              const SizedBox(height: 10),
-                              Text(
-                                department['name'],
-                                style: GoogleFonts.orbitron(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              Column(
-                                children: department['divisions']
-                                    .map<Widget>((division) => Padding(
-                                          padding: const EdgeInsets.symmetric(vertical: 4.0),
-                                          child: Text(
-                                            division,
-                                            style: GoogleFonts.orbitron(
-                                              fontSize: 18,
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                        ))
-                                    .toList(),
-                              ),
-                            ],
-                          ),
-                        ),
-                    ],
-                  ),
-                ],
-              );
-            },
-          ),
+    if (_isLoading) {
+      return Container(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        color: cardBackgroundColor,
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            _buildDepartmentList(),
+          ],
         ),
+      ),
+    );
+  }
+
+  void _showAddDepartmentDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String title = '';
+        return AlertDialog(
+          backgroundColor: cardBackgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          title: Text(
+            'Add Department',
+            style: TextStyle(
+              color: primaryColor,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                onChanged: (value) {
+                  title = value;
+                },
+                decoration: InputDecoration(hintText: "Title"),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: Text(
+                'Cancel',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(primaryColor),
+                padding: WidgetStateProperty.all<EdgeInsets>(
+                    EdgeInsets.symmetric(horizontal: 20, vertical: 10)),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              onPressed: () {
+                _addDepartment(title);
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _addDepartment(String title) async {
+    final headers = await getAuthHeader();
+
+    final CreateDepartmentRequest createDepartmentRequest =
+        CreateDepartmentRequest((b) => b..title = title);
+  }
+
+  Widget _buildDepartmentList() {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: departments.length,
+      itemBuilder: (context, index) {
+        final department = departments[index];
+        return _buildDepartmentItem(department);
+      },
+    );
+  }
+
+  Widget _buildDepartmentItem(GetDepartments200ResponseInner department) {
+    int departmentIndex = departments.indexOf(department);
+    _dropdownOpen.putIfAbsent(departmentIndex, () => false);
+    return Card(
+      color: cardBackgroundColor,
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(icons[department.logo] ?? Icons.error,
+                color: department.color != null
+                    ? cssColorToColor(department.color!)
+                    : Colors.grey),
+            title: Text(department.title.toString(),
+                style: TextStyle(
+                  color: department.color != null
+                      ? cssColorToColor(department.color!)
+                      : Colors.grey,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                )),
+            subtitle: Text(department.motto.toString()),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(_dropdownOpen[departmentIndex] == true
+                      ? Icons.arrow_drop_up
+                      : Icons.more_vert),
+                  onPressed: () {
+                    setState(() {
+                      _dropdownOpen[departmentIndex] =
+                          !_dropdownOpen[departmentIndex]!;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+          if (_dropdownOpen[departmentIndex] == true)
+            _buildDropdownContent(department),
+        ],
+      ),
+    );
+  }
+
+  void _showDeleteDialog(GetDepartments200ResponseInner department) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String confirmation = '';
+        return AlertDialog(
+          backgroundColor: cardBackgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          title: Text(
+            'Deleting ${department.title.toString()}',
+            style: TextStyle(
+              color: primaryColor,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          content: TextField(
+            onChanged: (value) {
+              confirmation = value;
+            },
+            decoration: InputDecoration(hintText: "Enter title to delete"),
+            keyboardType: TextInputType.number,
+            obscureText: true,
+          ),
+          actions: [
+            TextButton(
+              child: Text(
+                'Cancel',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(Colors.red),
+                padding: WidgetStateProperty.all<EdgeInsets>(
+                    EdgeInsets.symmetric(horizontal: 20, vertical: 10)),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              onPressed: () {
+                if (confirmation == department.title.toString()) {
+                  _deleteDepartment(department);
+                  Navigator.of(context).pop();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Title does not match'),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                }
+              },
+              child: Text(
+                'Delete',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteDepartment(GetDepartments200ResponseInner department) async {
+    final headers = await getAuthHeader();
+
+    final DeleteDepartmentRequest deleteDepartmentRequest =
+        DeleteDepartmentRequest((b) => b..departmentTitle = department.title);
+  }
+
+  Widget _buildDropdownContent(GetDepartments200ResponseInner department) {
+    Color currentColor = department.color != null
+        ? cssColorToColor(department.color!)
+        : Colors.grey;
+    String title = department.title ?? '';
+    String motto = department.motto ?? '';
+    String logo = department.logo ?? '';
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildDetailRow('Heads', department.heads.toString()),
+          _buildDetailRow('Proxys', department.proxys.toString()),
+          SizedBox(height: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 5),
+              ...department.divisions!.map((division) => ListTile(
+                    leading: Icon(icons[department.logo] ?? Icons.error,
+                        color: department.color != null
+                            ? cssColorToColor(department.color!)
+                            : Colors.grey),
+                    title: Text(division.toString(),
+                        style: TextStyle(
+                          color: department.color != null
+                              ? cssColorToColor(department.color!)
+                              : Colors.grey,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    subtitle: Text(department.motto.toString()),
+                  )),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _updateDepartment(GetDepartments200ResponseInner department, Color color,
+      String title, String motto, String logo) async {
+    final headers = await getAuthHeader();
+
+    final UpdateDepartmentRequest updateDepartmentRequest =
+        UpdateDepartmentRequest((b) => b
+          ..departmentTitle = department.title
+          ..color = colorToCssColor(color)
+          ..newTitle = title
+          ..motto = motto
+          ..logo = logo);
+  }
+
+  Widget _buildDetailRow(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 120, // Adjust width as needed
+            child: Text(label, style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+          Text(value),
+        ],
       ),
     );
   }
